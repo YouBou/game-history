66aa2e375527d03f998322e93a81f926dfdf2b14
ubow <ikas2lite@gmail.com>
2025-02-13 21:24:44 +0900

    [fix] db名修正

--- backend/laravel/config/database.php
+++ backend/laravel/config/database.php
@@ -34,7 +34,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'game-history'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
