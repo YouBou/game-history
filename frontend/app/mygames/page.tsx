e6fc88af4db4d3467ccfd12524273b4d703621f1
ubow <ikas2lite@gmail.com>
2025-02-20 20:25:23 +0900

    [feat] 一覧画面の仮実装

--- frontend/app/mygames/page.tsx
+++ frontend/app/mygames/page.tsx
@@ -1,9 +1,38 @@
-export default function Page()
-{
+'use client'
+
+import { useState, useEffect } from 'react';
+import mygamesData from './sample/mygames.json';
+type MygameData = {
+    title: string;
+    hard: string;
+    releaseDate: string;
+};
+export default function Page() {
+    const [data, setData] = useState<MygameData[]>([]);
+
+    useEffect(() => {
+        setData(mygamesData);
+    }, []);
+
     return (
         <>
-            <h2>My Games</h2>
-            <p>My Game List</p>
+            <h2>ゲーム一覧</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>タイトル</th>
+                        <th>ハード</th>
+                        <th>リリース日</th>
+                    </tr>
+                </thead>
+                {data.map((data: any) => (
+                    <tr key={data.title}>
+                        <td>{data.title}</td>
+                        <td>{data.hard}</td>
+                        <td>{data.releaseDate}</td>
+                    </tr>
+                ))}
+            </table>
         </>
     )
 }
