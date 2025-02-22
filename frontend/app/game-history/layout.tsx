9873e2d094ecb7760369d6164d387057963e5051
ubow <ikas2lite@gmail.com>
2025-02-22 23:40:09 +0900

    [feat] サイドバーを開閉可能にする

--- frontend/app/game-history/layout.tsx
+++ frontend/app/game-history/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 import {
     Box,
     AppBar,
@@ -12,78 +14,113 @@ import {
     List,
     ListItem,
     ListItemButton,
+    ThemeProvider,
+    createTheme
 } from "@mui/material";
-
 import MenuIcon from '@mui/icons-material/Menu';
+
+declare module "@mui/material/styles" {
+    interface BreakpointOverrides {
+        xs: false;
+        sm: false;
+        md: false;
+        lg: false;
+        xl: false;
+        mobile: true;
+        desktop: true;
+    }
+}
+
+const defaultTheme = createTheme({
+    breakpoints: {
+        values: {
+            mobile: 0,
+            desktop: 600,
+        },
+    },
+});
 
 export default function MygamesLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="fixed" >
-                <Toolbar>
-                    <IconButton>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="div"
-                        sx={{ flexGrow: 1 }}
-                    >
-                        GameHistory
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <Drawer anchor="left">
-                <Box sx={{ width: 240 }}>
-                    <Toolbar />
-                    <Divider />
-                    <List>
-                        <ListItem component="a" href="/game-history/mygames" disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="My Games" />
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider />
-                        <ListItem component="a" href="/game-history/games" disablePadding>
-                            <ListItemButton>
-                                <ListItemText primary="Games" />
-                            </ListItemButton>
-                        </ListItem>
-                        <Divider />
-                    </List>
-                </Box>
-            </Drawer>
-            <Box
-                component="main"
-                sx={{
-                    flexGrow: 1,
-                    p: 3,
-                    marginTop: "64px",
-                    width: "100%",
-                    background: 'white',
-                }}
-            >
-                {children}
-            </Box>
-            <Box
-                component='footer'
-                sx={{
-                    width: '100%',
-                    position: 'fixed',
-                    textAlign: 'center',
-                    bottom: 0,
-                    background: '#1976d2',
-                }}
-            >
-                <Typography variant='caption' sx={{ color: 'white' }}>
-                    &copy; 2025 GameHistory
-                </Typography>
-            </Box>
+    const [open, setOpen] = useState(false);
+
+    const toggleDrawer = (open: boolean) => {
+        setOpen(open);
+    }
+
+    const router = useRouter();
+
+    const list = () => (
+        <Box sx={{ width: 240 }}>
+            <Toolbar />
+            <Divider />
+            <List>
+                <ListItem component="a" href="/game-history/mygames" disablePadding>
+                    <ListItemButton>
+                        <ListItemText primary="My Games" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem component="a" href="/game-history/games" disablePadding>
+                <ListItemButton>
+                        <ListItemText primary="Games" />
+                    </ListItemButton>
+                </ListItem>
+                <Divider />
+            </List>
         </Box>
+    );
+
+    return (
+        <ThemeProvider theme={defaultTheme}>
+            <Box sx={{ display: 'flex' }}>
+                <AppBar position="fixed" >
+                    <Toolbar>
+                        <IconButton onClick={() => toggleDrawer(true)}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ flexGrow: 1 }}
+                        >
+                            GameHistory
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer open={open} onClose={() => toggleDrawer(false)} anchor="left">
+                    {list()}
+                </Drawer>
+                <Box
+                    component="main"
+                    sx={{
+                        flexGrow: 1,
+                        p: 3,
+                        marginTop: "64px",
+                        width: "100%",
+                        background: 'white',
+                    }}
+                >
+                    {children}
+                </Box>
+                <Box
+                    component='footer'
+                    sx={{
+                        width: '100%',
+                        position: 'fixed',
+                        textAlign: 'center',
+                        bottom: 0,
+                        background: '#1976d2',
+                    }}
+                >
+                    <Typography variant='caption' sx={{ color: 'white' }}>
+                        &copy; 2025 GameHistory
+                    </Typography>
+                </Box>
+            </Box>
+        </ThemeProvider>
     );
 }
