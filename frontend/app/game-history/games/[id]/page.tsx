df4f1ce535eea87ebf01eb8b0fba21762b65e8fb
ubow <ikas2lite@gmail.com>
2025-02-26 22:38:37 +0900

    [feat] フロント側のapi繋ぎ込み

--- frontend/app/game-history/games/[id]/page.tsx
+++ frontend/app/game-history/games/[id]/page.tsx
@@ -1,9 +1,51 @@
-export default function Page()
-{
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type GameData = {
+    name: string;
+    cover: {
+        url: string;
+    }
+}
+
+export default function Page() {
+    const [game, setGame] = useState<GameData>({
+        name: '',
+        cover: {
+            url: ''
+        }
+    });
+
+    useEffect(() => {
+        const fetchGame = async () => {
+            const pathParts = window.location.pathname.split('/');
+            const gameId = pathParts[pathParts.length - 1];
+
+            const res = await axios.get(`http://127.0.0.1:8000/api/games/${gameId}`);
+            setGame(res.data);
+        };
+        fetchGame();
+    }, []);
+
     return (
         <>
             <h2>Game</h2>
             <p>Game Info</p>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>タイトル</th>
+                        <th>カバー</th>
+                    </tr>
+                </thead>
+                <tr>
+                    <td>{game.name}</td>
+                    <td><img src={game.cover.url} /></td>
+                </tr>
+            </table>
         </>
     )
 }
